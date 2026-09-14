9c7a179280675c780178ce152cc3c2fdb91f73bd
teenytinybit <juliana.toporkova@gmail.com>
2026-09-14 13:08:57 +0300

    connect to message broker; add dockerfile

---
+++ src/internal/pubsub/publish.ts
@@ -0,0 +1,11 @@
+import type { ConfirmChannel } from "amqplib";
+
+export async function publishJSON<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const serialized = Buffer.from(JSON.stringify(value));
+  ch.publish(exchange, routingKey, serialized, { contentType: "application/json" });
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,26 @@
+import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+
+const connStr = "amqp://guest:guest@localhost:5672/";
+
 async function main() {
   console.log("Starting Peril server...");
+  const connection = await amqp.connect(connStr);
+  console.log("Connected to RabbitMQ");
+
+  const channel = await connection.createConfirmChannel();
+  console.log("Created confirm channel");
+
+  await publishJSON(channel, ExchangePerilDirect, PauseKey, {
+    isPaused: true,
+  } as PlayingState);
+
+  process.on("SIGINT", () => {
+    console.log("Shutting down...");
+    connection.close();
+  });
 }
 
 main().catch((err) => {
